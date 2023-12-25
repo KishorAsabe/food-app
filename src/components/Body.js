7bb51659d59801440722ab0b5c254f5092f496ef
KishorAsabe <kishorasabe2020@gmail.com>
2023-12-25 20:57:02 +0530

    search functionality added

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,16 +1,45 @@
 import RestaurantCard from "./ResturantCard"
 import {restaurantList}  from "../config"
+import { useState } from "react";
 
 
-// Body Component for body section: It contain all restaurant cards
-// We are mapping restaurantList array and passing data to RestaurantCard component as props with unique key as index
+
+
 const Body = () => {
+   
+    // making two local state variable 
+     const[restaurant,setRestaurant] = useState(restaurantList)  //store restolist
+     const[searchInput, setSearchInput] = useState() // store input text
+
+
+    //filter the resto based on input text    
+    const filter = (searchInput,restaurant)=>{
+          
+       return  restaurant.filter((restaurant)=> restaurant.data.name.toLowerCase().includes(searchInput))
+    }
+    
     return (
-      <div className="restaurant-list">
-        {restaurantList.map((restaurant) => {
-          return <RestaurantCard key={restaurant.data.id} {...restaurant.data} />;
-        })}
-      </div>
+        <>
+            <div className="search-container">
+            {/* serach feild captur then changing value */}
+             <input type="text" className="search-input" placeholder="search" value={searchInput}
+                onChange={(e)=>{setSearchInput(e.target.value)}}
+             />
+
+             {/* button */}
+             <button onClick={()=>{
+                const data = filter(searchInput,restaurant);
+                setRestaurant(data)
+             }}>search {searchInput}</button>
+            </div>
+            
+              
+            <div className="restaurant-list">
+                {restaurant.map((restaurant) => {
+                return <RestaurantCard key={restaurant.data.id} {...restaurant.data} />; // in data we have multiple elemet like name,cusines , rating thts why we use spread operator
+                })}
+            </div>
+        </>
     );
   };
 
