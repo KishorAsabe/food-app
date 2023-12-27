a02b6a822b33b4262351a064c8127c3f02c3d7cb
KishorAsabe <kishorasabe2020@gmail.com>
2023-12-27 23:21:27 +0530

    react strict added

--- src/index.js
+++ src/index.js
@@ -1,41 +1,10 @@
 
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
 import Header from "./components/Header";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
-
-
-
-/* My Food App structure will look like this, 
-            1) Header
-                - Logo
-                - Nav Items(right side)
-                - Cart
-            2) Body
-                - Search bar
-                - Restaurants List
-                    - Restaurant card
-                        - Image
-                        - Name
-                        - Rating
-            3) Footer
-                - Links
-                - Copyrights
-       
-*/
-
-// Title component for display logo
-
-
-// RestaurantList is JSON Data for displaying cards
-
-
-// Restaurant card component: Image, name, cuisine
-
-  
-
 
 
 
@@ -51,4 +20,6 @@ const AppLayout = () => {
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+root.render(  <React.StrictMode>
+  <AppLayout />
+</React.StrictMode>);
