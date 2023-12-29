b839c76e3d9291d6dacc4031c5c7882048308dd4
KishorAsabe <kishorasabe2020@gmail.com>
2023-12-30 00:54:33 +0530

    new pages added

---
+++ pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = ()=>{
+    return(
+    <div>
+       <h1>welocome to about page</h1>
+    </div>
+    )
+}
+
+export default About;
+
---
+++ pages/Error.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import { useRouteError } from "react-router-dom";
+
+const Error = ()=> {
+    // const err = useRouteError();
+    // console.log(err)
+    return(
+        <>
+            <h1>
+            404 not found</h1>
+        </>
+    )
+}
+
+export default Error;
