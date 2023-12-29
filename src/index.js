2fce1dc8f6fa3997ab2a8bbbae3ee05fb39ef6e7
KishorAsabe <kishorasabe2020@gmail.com>
2023-12-30 00:56:05 +0530

    browserRouter added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 
 import Header from "./components/Header";
-import Body from "./components/Body";
 import Footer from "./components/Footer";
+
+import About from "../pages/About";
+import Contact from "../pages/Contact";
+import Error from '../pages/Error'
+import Body from "./components/Body";
+import Restmenu from './components/Restmenu'
+
+
 
 
 
@@ -13,13 +20,34 @@ const AppLayout = () => {
   return (
     <React.Fragment>
       <Header />
-      <Body />
+      <Outlet /> {/* This will render child routes */}
       <Footer />
     </React.Fragment>
   );
 };
 
+const Body = () => {
+  return <div>This is the body content!</div>;
+};
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AppLayout />}>
+          <Route index element={<Body />} /> {/* This is nested under AppLayout */}
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="restaurant/:id" element={<Restmenu/>}/>
+          <Route path="*" element={<Error />} />
+        </Route>
+
+       
+
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(  <React.StrictMode>
-  <AppLayout />
-</React.StrictMode>);
+root.render(<App />);
