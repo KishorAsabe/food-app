937173ba09fccaaf3c4fdad2f679e4f33949df31
KishorAsabe <kishorasabe2020@gmail.com>
2024-01-05 12:08:34 +0530

    event listner removed

--- Utils/Hooks/useOnline.js
+++ Utils/Hooks/useOnline.js
@@ -6,15 +6,39 @@ const useOnline = ()=>{
 //make local varible to manage state 
 const[isOnline , setIsOnline] = useState(true);
 
+const handelOnline = ()=>
+{
+    setIsOnline(true);
+}
+
+const handelOffline = ()=>
+{
+    setIsOnline(false);
+}
 //we want set online or offline  based on their system status
 //when paege first time render 
 useEffect(()=>{
-     window.addEventListener('online',()=>setIsOnline(true));
-     window.addEventListener('offline',()=>setIsOnline(false));
+     window.addEventListener('online',handelOnline);
+     window.addEventListener('offline',handelOffline);
+
+     
+//remove event listner 
+
+return ()=>{
+    window.removeEventListener('online',handelOnline);
+    window.removeEventListener('offline',handelOffline);
+}
+
 },[])
+
 
 return isOnline;
 
 
 }
  export default useOnline;
+
+
+ // when add eventlister or setinterval or setimout like async funtion  
+// then we need to remove before unmounting then componet 
+//means before leaving the page it should get clean otherwise it impact on performance 
