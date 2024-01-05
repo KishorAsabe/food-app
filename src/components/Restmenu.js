c50082eb878fb4e3a7418e14f3d4fd17d45a4770
KishorAsabe <kishorasabe2020@gmail.com>
2024-01-05 10:54:26 +0530

    useRstaurant custom hook created

--- src/components/Restmenu.js
+++ src/components/Restmenu.js
@@ -1,58 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { swiggy_menu_api, RESTAURANT_TYPE_KEY,IMG_CDN_URL,DISH_ITEM_TYPE_KEY,MENU_ITEM_TYPE_KEY } from '../config';
 import Shimmer from "./Shimmer"
+import { swiggy_menu_api, RESTAURANT_TYPE_KEY,IMG_CDN_URL,DISH_ITEM_TYPE_KEY,MENU_ITEM_TYPE_KEY } from '../../src/config';
+
+import UseRestaurant from '../../Utils/Hooks/useRestaurant';
 
   
 const Restmenu = () => {
   const { id } = useParams();
-  const [restaurant, setRestaurant] = useState(null);
-  const [menuItems, setMenuItems] = useState([]);
-
-
-useEffect(()=>{
-  getRestomenu()
-},[])
-
-  async function getRestomenu() {
-    try {
-      const response = await fetch(swiggy_menu_api + id);
-      const json = await response.json();
-      console.log(json)
-      const restaurantData1 = json?.data?.cards?.map(x => x.card)
-      const restaurantData = restaurantData1?.find(x => x && x.card['@type'] === RESTAURANT_TYPE_KEY)?.card?.info || null;
-     // console.log(restaurant)
-     
-
-     
-      const menuItemsData = json?.data?.cards?.find((x)=>x.groupedCard)?.groupedCard?.cardGroupMap?.REGULAR?.cards?.map((x)=>x.card?.card)?.filter(x=>x["@type"] == MENU_ITEM_TYPE_KEY)?.map(x => x.itemCards).flat()?.map(x => x.card?.info) ;
-     
-      // const menuItemsData1 = json?.data?.cards?.find((x)=>x.groupedCard)?.groupedCard?.cardGroupMap?.REGULAR?.cards.find(x =>x.card?.card["@type"] == MENU_ITEM_TYPE_KEY)?.card?.card?.itemCards?.map((x)=>x.card?.info)
-      // console.log("menuItemsData1",menuItemsData1)
-      //to remove unique id warning
-      const uniqueMenuItems = [];
-      menuItemsData.forEach((item) => {
-        if (!uniqueMenuItems.find(x => x.id === item.id)) {
-          uniqueMenuItems.push(item);
-        }
-      })
-      setMenuItems(uniqueMenuItems);
-     
-      setRestaurant(restaurantData)
-     
- 
-      
-     
-     // console.log("menuItems",menuItems)
-      
-    } catch (e) {
-      console.error(e);
-      setMenuItems([]);
-      setRestaurant(null);
-    }
-  }
 
   
+
+  //we use hook to get  Rstaurant
+
+  const {restaurant,menuItems} = UseRestaurant(id);
+
+
+
   return (!restaurant  )?<Shimmer/>:(
     
     <div className="menu">
